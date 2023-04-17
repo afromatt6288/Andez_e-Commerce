5d0ac90e06e27d9ad23590d869a9998f652a4372
Matthew Clark <afromatt6288@hotmail.com>
2023-04-17 11:45:57 -0600

    Updates to config to allow for .env. Updated Models with User and validations. Update app with login/signup routes.

--- server/app.py
+++ server/app.py
@@ -2,16 +2,92 @@
 
 import os 
 from flask import jsonify, make_response, request, g, current_app, redirect, abort, session
-from flask_migrate import Migrate
 from flask_restful import Resource
 import json
 from sqlalchemy.exc import IntegrityError
 from config import app,bcrypt,db,api
 from models import User, Classname
 
-                       ###################   INSERT   ################## 
-  ## See further Below ###### Routes, GET, POST, PATCH, DELETE, Etc ####  ## See further Below ##
-                       ####################   HERE   ###################
+    ###########################################
+    ##                Home API               ##
+    ###########################################
+
+class Home(Resource):
+    def get(self):
+        andez_dict = '''
+            <h1>"message": "Welcome to the Andez RESTful API"</h1>
+        '''
+        response = make_response(andez_dict, 200)
+        return response
+api.add_resource(Home, '/')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+    ###########################################
+    ##            Logging in/Out             ##
+    ##   Session, Authenticating, Password   ##
+    ###########################################
+
+@app.before_request
+def check_if_logged_in():
+    access_list = ['clear', 'signup', 'check_session', 'login', ]
+    if (request.endpoint) not in access_list and (not session.get('user_id')):   
+        return {'error': 'Unauthorized'}, 401
+
+class ClearSession(Resource):
+    def delete(self):    
+        session['page_views'] = None
+        session['user_id'] = None
+        return {}, 204
+api.add_resource(ClearSession, '/clear', endpoint='clear')  
+
+class Signup(Resource):
+    def post(self):
+        username = request.get_json()['username']  
+        password = request.get_json()['password']
+        if username and password:
+            new_user = User(username=username)
+            new_user.password_hash = password
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                session['user_id'] = new_user.id
+                return new_user.to_dict(), 201
+            except IntegrityError:
+                return {'error': '422 Unprocessable Entity'}, 422
+        return {'error': '422 Unprocessable Entity'}, 422
+api.add_resource(Signup, '/signup', endpoint='signup')
+
+class CheckSession(Resource):
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session.get('user_id')).first()
+            return user.to_dict(), 200  ## OR ## return jsonify(user.to_dict()), 200 ##
+        return {'message': '401 Unauthorized'}, 401  ## OR ## return jsonify({'message': '401: Not Authorized'}), 401 ##
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+
+class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        user = User.query.filter(User.username == username).first()
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200     ## OR ## return jsonify(user.to_dict()), 200 ##
+        return {'error': '401 Unauthorized'}, 401
+api.add_resource(Login, '/login', endpoint='login')
+
+class Logout(Resource):
+    def delete(self): 
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {'message': '204: No Content'}, 204  ## OR ## return jsonify({'message': '204: No Content'}), 204 ##
+        return {'error': '401 Unauthorized'}, 401
+api.add_resource(Logout, '/logout')
+
+    ###########################################
+    ##        GET, POST, PATCH, DELETE       ##
+    ###########################################
+
--- server/models.py
+++ server/models.py
@@ -4,6 +4,113 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from config import bcrypt,db
 
-                       ###################   INSERT   ################## 
-  ## See further Below ###########           CLASSES           #########  ## See further Below ##
-                       ####################   HERE   ###################
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-transactions.user', '-created_at', '-updated_at',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    shipping_address = db.Column(db.String, nullable=False)
+    account_balance = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))               # utf-8 encoding and decoding is required in python 3
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    @validates('username')
+    def validate_username(self, key, username):
+        users = User.query.all()
+        usernames = [user.username for user in users]
+        if not username:
+            raise ValueError("User must have a username")
+        elif username in usernames:
+            raise ValueError("User Username must be unique")
+        return username
+
+    @validates('email')
+    def validate_email(self, key, email):
+        if not email:
+            raise ValueError("User must have a email")
+        if '@' not in email:
+            raise ValueError("User failed simple email validation")
+        return email
+
+    @validates('shipping_address')
+    def validate_shipping_address(self, key, shipping_address):
+        if not shipping_address:
+            raise ValueError("User must have a shipping_address")
+
+    @validates('account_balance')
+    def validate_account_balance(self, key, account_balance):
+        if not account_balance:
+            raise ValueError("User must have an account_balance.")
+        elif int(account_balance) >= 0:
+            raise ValueError("User account_balance cannot be negative.")
+        return account_balance
+    
+    def __repr__(self):
+        return f'User ID: {self.id}, Username: {self.username}, Email: {self.email}, Address: {self.shipping_address}, Account Balance: {self.account_balance}'
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = 'items'
+
+    # serialize_rules = ('-created_at', '-updated_at',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    ## still need name, price, description, stretch-(vendor_id)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    pass
+
+class Transaction(db.Model, SerializerMixin):
+    __tablename__ = 'transactions'
+
+    # serialize_rules = ('-created_at', '-updated_at',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    ## still need user_id, item_id, item_price, transaction_date, refund, update_date, (total price will be a front end function)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    pass
+
+## Stretch
+class Vendor(db.Model, SerializerMixin):
+    __tablename__ = 'vendors'
+
+    # serialize_rules = ('-created_at', '-updated_at',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    ## still need vendor_name
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    pass
+
+class Vendor_Item(db.Model, SerializerMixin):
+    __tablename__ = 'vendor_items'
+
+    # serialize_rules = ('-created_at', '-updated_at',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    ## still need vendor_id, item_id
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    pass
