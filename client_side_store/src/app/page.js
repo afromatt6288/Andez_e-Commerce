569a918f3819eb7ad8bab0b62898fe649b35cfcb
PrestonHunter <prestonhunter2018@gmail.com>
2023-04-17 13:24:52 -0600

    fixed dumb import

--- client_side_store/src/app/page.js
+++ client_side_store/src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from 'next/image.js'
-import { Inter } from 'next/font/google/index.js'
+import { Inter } from 'next/font/google'
 import Mainpage from "./Mainpage.js"
 import { Main } from 'next/document.js'
 const inter = Inter({ subsets: ['latin'] })
