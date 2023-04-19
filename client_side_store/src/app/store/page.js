f4a4549c995412c03ecf933b46d8c50bb8d33354
Matthew Clark <afromatt6288@hotmail.com>
2023-04-18 18:33:47 -0600

    deleted client_side_store folder, to start over.

--- client_side_store/src/app/store/page.js
+++
@@ -1,5 +0,0 @@
-"use client";
-import Store from "../components/StorePage/Store"
-export default function Page({params}) {
-    return <Store/>;
-}
--- client_side_store/src/app/testing/page.js
+++
@@ -1,4 +0,0 @@
-import Mainpage from "../Mainpage.js"
-export default function Page({params}) {
-    return <Mainpage/>;
-}
